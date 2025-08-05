10048bcc154c5ca7fdf5af5c359cf3a84e705540
Rudra Rautray <rudraprasadrautray2002@gmail.com>
2025-08-05 05:21:07 +0000

    u

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,27 +1,50 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/ui/model/json/JSONModel"
-], (Controller, JSONModel) => {
+    "sap/ui/model/json/JSONModel",
+    "sap/m/MessageToast"
+], function (Controller, JSONModel, MessageToast) {
     "use strict";
 
     return Controller.extend("btp.project1.controller.View1", {
-        onInit() {
+        onInit: function () {
             var that = this;
+            $.ajax({
+                url: "./Northwind/V2/Northwind/Northwind.svc/Products?$format=json",
+                method: "GET",
+                dataType: "json",
+                success: function (data) {
+                    var oModel = new JSONModel(data);
+                    that.getView().setModel(oModel, "Products");
+                    // MessageToast.show("Data loaded successfully!");
+                },
+                error: function () {
+                   // MessageToast.show("Failed to load dashboard data.");
+                }
+            });
 
-        $.ajax({
-            url: "./Northwind/V2/Northwind/Northwind.svc/Products?$format=json",
-            method: "GET",
-            dataType: "json",
-            success: function (data) {
-                const dashboardModel = new sap.ui.model.json.JSONModel(data.value);
-                that.getView().setModel(dashboardModel, "Products");
-                sap.m.MessageToast.show("Data coming successfully!");
-            },
-            error: function () {
-                sap.m.MessageToast.show("Failed to load dashboard data.");
-            }
-        });
 
+            var myKeyVals = { 
+                    "current_status": "available",
+                    "room_category": "Suite ",
+                    "room_feature": "Sea Facing",
+                    "room_no": 850
+               
+            };
+            
+            $.ajax({
+                method: "POST",
+                url: "./API/api/rooms",
+                data: JSON.stringify(myKeyVals),
+                contentType: "application/json",   
+                dataType: "json",             
+                success: function (_data) {
+                    MessageToast.show("Data created successfully!");
+                },
+                error: function () {
+                    MessageToast.show("Failed to load dashboard data.");
+                }
+            });
+            
         }
     });
 });
